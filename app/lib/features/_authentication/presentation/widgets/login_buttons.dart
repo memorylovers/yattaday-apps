1721ec30c6810e6ad4172fa3b51dafcc6a249675
memory-lovers <8280057+memory-lovers@users.noreply.github.com>
2025-06-09 13:24:58 +0900

    feat: LoginButtonにセマンティクスIDを追加

--- app/lib/features/_authentication/presentation/widgets/login_buttons.dart
+++ app/lib/features/_authentication/presentation/widgets/login_buttons.dart
@@ -7,23 +7,28 @@ class LoginButton extends StatelessWidget {
     required this.icon,
     required this.label,
     this.onPressed,
+    this.semanticsId,
   });
 
   final bool isLoading;
   final IconData icon;
   final String label;
   final VoidCallback? onPressed;
+  final String? semanticsId;
 
   @override
   Widget build(BuildContext context) {
-    return ElevatedButton.icon(
-      onPressed: isLoading ? null : onPressed,
-      icon: Icon(icon),
-      label: Text(label),
-      style: ElevatedButton.styleFrom(
-        iconSize: 28,
-        textStyle: TextStyle(fontSize: 18),
-        padding: EdgeInsets.symmetric(vertical: 8, horizontal: 8),
+    return Semantics(
+      identifier: semanticsId,
+      child: ElevatedButton.icon(
+        onPressed: isLoading ? null : onPressed,
+        icon: Icon(icon),
+        label: Text(label),
+        style: ElevatedButton.styleFrom(
+          iconSize: 28,
+          textStyle: TextStyle(fontSize: 18),
+          padding: EdgeInsets.symmetric(vertical: 8, horizontal: 8),
+        ),
       ),
     );
   }
