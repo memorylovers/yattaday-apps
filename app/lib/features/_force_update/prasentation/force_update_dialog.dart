5ecc1f40a336dc0a65be775ae82ab0c406074355
memory-lovers <8280057+memory-lovers@users.noreply.github.com>
2025-06-09 20:01:29 +0900

    fix: force_update_dialog.dartの未使用変数を削除してlintエラーを修正

--- app/lib/features/_force_update/prasentation/force_update_dialog.dart
+++ app/lib/features/_force_update/prasentation/force_update_dialog.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-import '../../../_gen/i18n/strings.g.dart';
+// TODO: 実装時に有効化
+// import '../../../_gen/i18n/strings.g.dart';
 
 class ForceUpdateDialog extends HookConsumerWidget {
   const ForceUpdateDialog({super.key});
@@ -20,9 +21,10 @@ class ForceUpdateDialog extends HookConsumerWidget {
     // TODO: not implement
     throw UnimplementedError();
 
-    final title = i18n.forceUpdate.title;
-    final message = i18n.forceUpdate.desc;
-    final btnLabel = i18n.forceUpdate.label;
+    // 以下のコードは実装時に使用予定
+    // final title = i18n.forceUpdate.title;
+    // final message = i18n.forceUpdate.desc;
+    // final btnLabel = i18n.forceUpdate.label;
 
     // return PopScope(
     //   canPop: false,
